6897e1d4075f18a6f136ac69edb9d7b11af8bdaa
Julien <julien.nowalczyk@gmail.com>
2018-02-27 16:25:18 +0100

    fix env var in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,11 @@
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+
 provider "azurerm" {
-    subscription_id = "$${ARM_SUBSCRIPTION_ID}"
-    client_id       = "$${ARM_CLIENT_ID}"
-    client_secret   = "$${ARM_CLIENT_SECRET}"
-    tenant_id       = "$${ARM_TENANT_ID}"
+    subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+    client_id       = "${var.ARM_CLIENT_ID}"
+    client_secret   = "${var.ARM_CLIENT_SECRET}"
+    tenant_id       = "${var.ARM_TENANT_ID}"
 }
